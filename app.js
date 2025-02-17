c05a075f8cc9a05f447b3b1452e5fea01ecc8878
s0ntyrr <santyrap12@gmail.com>
2025-02-17 09:42:04 -0500

    cambios

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+let nombreInput = document.getElementById('input-name');
+let adicionarBtn = document.getElementById('adicionar-btn');
+let listaNombres = document.getElementById('lista-nombres');
 
+adicionarBtn.addEventListener('click', function()){
+    let nombre = nombreInput.value;
+    if(nombre !== ''){
+        let li = document.createElement('li');
+        listaNombre.appendChild(li);
+        nombreInput.value = '';
+    } else{
+        alert ('Escriba un nombre')
+    }
+}
