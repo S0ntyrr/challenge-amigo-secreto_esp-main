b1044451812ad3c65f248f183d1a026d26085104
s0ntyrr <santyrap12@gmail.com>
2025-02-17 09:57:34 -0500

    Arreglo en funcion

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let nombreInput = document.getElementById('input-name');
 let adicionarBtn = document.getElementById('adicionar-btn');
 let listaNombres = document.getElementById('lista-nombres');
 
-adicionarBtn.addEventListener('click', function()){
+adicionarBtn.addEventListener('click', function() {
     let nombre = nombreInput.value;
     if(nombre !== ''){
         let li = document.createElement('li');
@@ -11,4 +11,4 @@ adicionarBtn.addEventListener('click', function()){
     } else{
         alert ('Escriba un nombre')
     }
-}
+}); 
